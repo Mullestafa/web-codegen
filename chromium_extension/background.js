c5b32d76b1ea56336a5eb01c5bf7b4909bb400f1
Mustafa H <mustafahekmat@hotmail.dk>
2023-06-21 23:58:16 +0200

    Added a venv

--- chromium_extension/background.js
+++ chromium_extension/background.js
@@ -1,13 +1,18 @@
 chrome.commands.onCommand.addListener(function (command) {
   if (command === "api_hotkey") {
-    // Make the API request here
     fetch("http://localhost:5000")
       .then(response => response.text())
       .then(data => {
-        console.log(data); // Placeholder response
+        showPopup(data); // Show the API response in a popup
       })
       .catch(error => {
         console.log("Error:", error);
       });
   }
 });
+
+function showPopup(response) {
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.tabs.sendMessage(tabs[0].id, { response: response });
+  });
+}
